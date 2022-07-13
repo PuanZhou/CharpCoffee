3a3bd9ee269c0b136bd6e97c6116b67278b4baf6
soujirofb <soujirofb@gmail.com>
2022-07-13 09:29:44 +0800

    及時客服DEMO鍵 客服人員顯示 "客服"

--- slnProduct_core/prjProduct_core/wwwroot/js/chat(backup).js
+++ slnProduct_core/prjProduct_core/wwwroot/js/chat(backup).js
@@ -5,11 +5,11 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
-    if (message == "") { return };
     let time = new Date();
     let hm = time.getHours() + ":" + (time.getMinutes() < 10 ? '0' : '') + time.getMinutes();
     let head = message.substr(0, 5);
     let body = message.substr(5);
+    console.log(body);
 
     if (head == "ihtgs") {
         if ($("#userInput")[0].value == user) {
@@ -22,37 +22,19 @@ connection.on("ReceiveMessage", function (user, message) {
                                         </div>`);
         }
     } else {
-        if ($("#userInput")[0].value == "李小明") {
-            if ($("#userInput")[0].value == user) {
-                $("#messagesList").prepend(`<div class="clearfix mt-1">
-                                        <div class="shadow rounded p-1 pull-right bg-light">
-                                            <h6>${hm}</h6><h4 class="text-success">客服：${message}</h4>
-                                        </div>
-                                    </div>`);
-            }
-            else {
-                $("#messagesList").prepend(`<div class="clearfix mt-1">
-                                        <div class="shadow rounded p-1 pull-left bg-primary">
-                                            <h6 class="text-white">${hm}</h6><h4 class="text-black">${user}：${message}</h4>
-                                        </div>
-                                    </div>`);
-            }
-        }
-        else {
-            if ($("#userInput")[0].value == user) {
-                $("#messagesList").prepend(`<div class="clearfix mt-1">
+        if ($("#userInput")[0].value == user) {
+            $("#messagesList").prepend(`<div class="clearfix mt-1">
                                         <div class="shadow rounded p-1 pull-right bg-light">
                                             <h6>${hm}</h6><h4 class="text-success">您：${message}</h4>
                                         </div>
                                     </div>`);
-            }
-            else {
-                $("#messagesList").prepend(`<div class="clearfix mt-1">
+        }
+        else {
+            $("#messagesList").prepend(`<div class="clearfix mt-1">
                                         <div class="shadow rounded p-1 pull-left bg-primary">
-                                            <h6 class="text-white">${hm}</h6><h4 class="text-black">客服：${message}</h4>
+                                            <h6 class="text-white">${hm}</h6><h4 class="text-black">${user}：${message}</h4>
                                         </div>
                                     </div>`);
-            }
         }
     }
 });
@@ -66,13 +48,10 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    document.getElementById("messageInput").value = "";
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
-
-
 
 
