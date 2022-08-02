38b1bab8bd0a06b5f01cc8509429d4faac8dd3b8
datejoy <mokuhyouhahon@gmail.com>
2022-08-02 19:13:26 +0800

    8/2 1.在電子報中加上網址 2.增加COUPON Controller 後臺派發優惠券機制 3.新增CouponViewModel 4.小修通知樣式(Layout跟MemCenter都有) 5. HomeController 修改 Create 6.新增CouponViewModel 7.修改MemberViewModel DateTime 8.Notice.cshtml 修改 css 9.QueryOrderPartial 新增css 10.金額詳細用div包住 span改成p 11.Layout: 通知li的class <li class="n_@item.HasRead">                     通知script增加已讀未讀樣式 12後台Layout1:新增優惠券管理 13.修改Notice.css樣式 14.Login 新註冊的Demo鍵 15.忘記密碼Email Demo鍵 16.修改密碼過後Flag恢復False

---
+++ slnProduct_core/prjProduct_core/ViewModel/CCouponViewModel.cs
@@ -0,0 +1,45 @@
+﻿using prjProduct_core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjProduct_core.ViewModel
+{
+    public class CCouponViewModel
+    {
+
+        public int CouponId { get; set; }
+
+
+        
+        [Required(ErrorMessage ="不得為空")]
+        [DisplayName("優惠券名稱")]
+        public string CouponName { get; set; }
+
+
+        [DisplayName("折抵金額")]
+        [Required(ErrorMessage = "不得為空")]
+        public decimal Money { get; set; }
+
+
+        [DisplayName("折抵條件")]
+        [Required(ErrorMessage = "不得為空")]
+        public int Condition { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayName("起始日期")]
+        public DateTime? CouponStartDate { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayName("結束日期")]
+        public DateTime? CouponDeadline { get; set; }
+
+        public virtual ICollection<CouponDetail> CouponDetails { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
